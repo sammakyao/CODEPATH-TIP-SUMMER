2aa6c06537a70192dc6a17177abe53f8a6cba2b9
Samwel Makyao <97218692+sammakyao@users.noreply.github.com>
2024-06-16 01:09:17 +0000

    8

--- main.py
+++ main.py
@@ -55,3 +55,23 @@ print(time)
 '''
 
 # Blackjack problem 
+
+'''
+def blackjack(score):
+  if score == 21:
+    print("Blackjack!")
+  elif score > 21:
+    print("Bust!")
+  elif score >= 17 and score <= 21:
+    print("Nice hand")
+  elif score < 17:
+    print("Hit me!")
+
+blackjack(24)
+blackjack(19)
+blackjack(21)
+blackjack(10)
+
+'''
+
+# 
