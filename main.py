ff6659f73b96f8fef2e0bcdd951243ed612f6959
Samwel Makyao <97218692+sammakyao@users.noreply.github.com>
2024-06-16 05:12:28 +0000

    16

--- main.py
+++ main.py
@@ -148,7 +148,7 @@ print(sum)
 '''
 
 # 15 - Negative numbers prints
-
+'''
 def print_negatives(lst):
   # for a certain number i in lst
   for i in lst:
@@ -159,3 +159,21 @@ def print_negatives(lst):
     else:
       return None
 print_negatives([1,2,3,5])
+
+'''
+'''
+Write a function greet_user() that takes in a string name as a parameter and prints "Hello <name>".
+
+def greet_user(name):
+    pass
+Example Input: Michael
+Example Output: Hello Michael
+
+'''
+'''
+def greet_user(name):
+  print("Hello " + name)
+
+greet_user("Michael")
+
+'''
