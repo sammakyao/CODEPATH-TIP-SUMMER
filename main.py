afa4f479de708d54839297ef0dc988d27d80b7aa
Samwel Makyao <97218692+sammakyao@users.noreply.github.com>
2024-06-16 00:50:10 +0000

    update

--- main.py
+++ main.py
@@ -3,6 +3,14 @@
 # then double the sum, then print the console
 
 
-def sum(a, b):
-  return a + b
-print(sum(20, 8) * 2)
+#def sum(a, b):
+#  return a + b
+#print(sum(20, 8) * 2)
+
+# write a function that returns product of 
+# two integers a and b
+
+def product(a, b):
+  return a * b
+result = product(22, 7)
+print(result)
